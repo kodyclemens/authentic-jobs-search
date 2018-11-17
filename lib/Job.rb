57f5347e1b75040a61f433f12a3e09ee133b33fb
kodyclemens <kody.clemens@gmail.com>
2018-11-17 14:17:29 -0500

    Moved functionality of creating jobs to the Job class.

--- lib/Job.rb
+++ lib/Job.rb
@@ -1,3 +1,7 @@
+require_relative "JobAPI.rb"
+require "JSON"
+require "pry"
+
 class Job
   attr_reader :id, :title, :location, :perks, :post_date, :apply_url
 
@@ -17,6 +21,30 @@ class Job
     @@all
   end
 
+  def self.create_jobs
+    id = ""
+    title = ""
+    location = "N/A"
+    perks = ""
+    post_date = ""
+    apply_url = ""
+
+    parsed_jobs_hash_json = JobAPI.get_all_jobs
+
+    parsed_jobs_hash_json["listings"]["listing"].each do |job_attr|
+      id = job_attr["id"]
+      title = job_attr["title"]
+      perks = job_attr["perks"]
+      # Check to ensure the hash includes a location key. Not all jobs within our hash contain a location.
+      if job_attr["company"]["location"]
+        location = job_attr["company"]["location"]["name"]
+      end
+      post_date = job_attr["post_date"]
+      apply_url = job_attr["apply_url"]
+      self.new(id, title, location, perks, post_date, apply_url)
+    end
+  end
+
   def self.search_by_location(location)
     selected_jobs = []
     @@all.each do |job_obj|
@@ -27,3 +55,10 @@ class Job
     return selected_jobs
   end
 end
+
+# Testing functionality. Binding cannot be the last line.
+Job.create_jobs
+binding.pry
+Job.search_by_location("New York")
+binding.pry
+puts "Please ignore"
--- lib/JobAPI.rb
+++ lib/JobAPI.rb
@@ -1,6 +1,4 @@
 require "open-uri"
-require "pry"
-require_relative "Job.rb"
 
 class JobAPI
   @@api = File.open("../api.txt", "r").read
@@ -12,33 +10,4 @@ class JobAPI
     parsed_jobs = JSON.parse(unparsed_jobs)
     return parsed_jobs
   end
-
-  def self.create_jobs(parsed_jobs_hash_json)
-    id = ""
-    title = ""
-    location = "N/A"
-    perks = ""
-    post_date = ""
-    apply_url = ""
-
-    parsed_jobs_hash_json["listings"]["listing"].each do |job_attr|
-      id = job_attr["id"]
-      title = job_attr["title"]
-      perks = job_attr["perks"]
-      # Check to ensure the hash includes a location key. Not all jobs within our hash contain a location.
-      if job_attr["company"]["location"]
-        location = job_attr["company"]["location"]["name"]
-      end
-      post_date = job_attr["post_date"]
-      apply_url = job_attr["apply_url"]
-      Job.new(id, title, location, perks, post_date, apply_url)
-    end
-  end
 end
-
-# Testing functionality. Binding cannot be the last line.
-JobAPI.create_jobs(JobAPI.get_all_jobs)
-binding.pry
-Job.search_by_location("New York")
-binding.pry
-puts "Please ignore"
