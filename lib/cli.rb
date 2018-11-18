881724c931e6d6f9cb1f0e7e3442356bb823387a
kodyclemens <kody.clemens@gmail.com>
2018-11-17 21:22:19 -0500

    Added CLI methods to introduce user to program, show user menu, get user input, validate user input and run the Job search_by_location method based on user selection.

--- lib/cli.rb
+++ lib/cli.rb
@@ -2,13 +2,46 @@ require_relative "Job"
 require "pry"
 
 class CLI
-  def run
+  attr_accessor :input
+
+  def self.run
     puts "Authentic Jobs Search"
     puts "A CLI tool to search and view details about jobs posted to authenticjobs.com"
     puts "-----"
     puts "Please standby while we collect the job postings..."
     Job.create_jobs
-    binding.pry
-    puts "ignore"
+    self.menu
+  end
+
+  def self.menu
+    puts "Please make a selection by entering a number:"
+    puts "1. Search jobs by location"
+    puts "2. Exit"
+    @input = gets.chomp.to_i
+    validate_input(@input)
+    run_selection(@input)
+  end
+
+  def self.validate_input(input)
+    if @input.is_a? Integer
+      return true
+    else
+      puts "Invalid selection!"
+      menu
+    end
+  end
+
+  def self.run_selection(input)
+    if validate_input(input) == true
+      case input
+      when 1
+        # Testing functionality of Job class method
+        Job.search_by_location("NY")
+      when 2
+        exit
+      end
+    end
   end
 end
+
+CLI.run
