eb2b74afd253bf889bcd7584898ab175ba6b00ff
kodyclemens <kody.clemens@gmail.com>
2018-11-17 20:39:20 -0500

    Added formatted output for Job class search_by_location method.

--- lib/Job.rb
+++ lib/Job.rb
@@ -21,7 +21,7 @@ class Job
   end
 
   def self.create_jobs
-    id = ""
+    id = 1
     title = ""
     location = "N/A"
     perks = ""
@@ -31,7 +31,6 @@ class Job
     parsed_jobs_hash_json = JobAPI.get_all_jobs
 
     parsed_jobs_hash_json["listings"]["listing"].each do |job_attr|
-      id = job_attr["id"]
       title = job_attr["title"]
       perks = job_attr["perks"]
       # Check to ensure the hash includes a location key. Not all jobs within our hash contain a location.
@@ -41,23 +40,46 @@ class Job
       post_date = job_attr["post_date"]
       apply_url = job_attr["apply_url"]
       self.new(id, title, location, perks, post_date, apply_url)
+      id += 1
     end
   end
 
   def self.search_by_location(location)
     selected_jobs = []
+    blank_line = ""
+    seperator = "-----"
+
+    # Find all jobs that included the search term in their location attribute.
     @@all.each do |job_obj|
       if job_obj.location.include?(location)
-        selected_jobs << job_obj
+        selected_jobs << ["#{job_obj.id}. #{job_obj.title} located in #{job_obj.location}.", "Apply at: #{job_obj.apply_url}"]
       end
     end
-    return selected_jobs
+
+    if selected_jobs.count > 1
+      puts "A total of #{selected_jobs.count} jobs were found for search term \"#{location}\":"
+      puts blank_line
+    elsif selected_jobs.count == 1
+      puts "One job was found for search term \"#{location}\":"
+      puts blank_line
+    else
+      puts "No jobs were found for search term \"#{location}\"."
+    end
+
+    # Output pre-formatted text for each job to the user.
+    selected_jobs.each do |job|
+      puts job[0]
+      puts blank_line
+      puts job[1]
+      puts blank_line
+      puts seperator
+      puts blank_line
+    end
+    return nil
   end
 end
 
-# Testing functionality. Binding cannot be the last line.
+# Testing Area. Binding cannot be the last line.
 Job.create_jobs
-binding.pry
-Job.search_by_location("New York")
 binding.pry
 puts "Please ignore"
