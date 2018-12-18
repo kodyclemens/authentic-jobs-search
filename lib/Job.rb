588a44e798b69d49af869087ee45797a43b752d1
kodyclemens <kody.clemens@gmail.com>
2018-12-18 00:29:07 -0500

    Seperation of concerns greatly improved. Job class is responsible for object creation and searching. CLI class handles user interaction and displaying of Job method return values

--- lib/Job.rb
+++ lib/Job.rb
@@ -1,10 +1,8 @@
-# TODO: Refactor to return select job objects, handle all printing in CLI
-require 'pry'
 class Job
   attr_reader :id, :title, :location, :perks, :post_date, :apply_url
 
   @@all = []
-  @@search_returned_job_ids = []
+  @@jobs_found_by_search = []
 
   def initialize(id, title, location = nil, perks = 'N/A', post_date, apply_url)
     @id = id
@@ -49,74 +47,32 @@ class Job
     end
   end
 
-  def self.search(type, search_term)
-    # Method is called and arguments passed from CLI
-    # .find_jobs will return formatted results based on search type
-    # and user's search term
-    CLI.clear_terminal
-    selected_jobs = find_jobs(type, search_term)
-    if selected_jobs.count > 0
-      output_jobs(selected_jobs, search_term)
+  def self.search(search_term)
+    clear_search_returned_jobs
+
+    @@jobs_found_by_search = @@all.select do |job|
+      job.location.include?(search_term)
+    end
+  end
+
+  def self.jobs_found_by_search
+    @@jobs_found_by_search
+  end
+
+  def self.clear_search_returned_jobs
+    @@jobs_found_by_search.clear
+  end
+
+  def self.return_job_by_index(user_input)
+    index = user_input.to_i - 1
+    @@jobs_found_by_search[index]
+  end
+
+  def self.jobs_found?
+    if @@jobs_found_by_search == []
+      false
     else
-      puts Rainbow("No jobs were found for search term \"#{search_term}\".").red.bright
-      CLI.menu
+      true
     end
-  end
-
-  def self.output_jobs(jobs, search_term)
-    blank_line = ''
-    seperator = '-----'
-
-    jobs.each do |job|
-      puts job[0]
-      puts blank_line
-      puts job[1]
-      puts blank_line
-      puts seperator
-      puts blank_line
-      sleep(0.5)
-    end
-    puts Rainbow("#{jobs.count} job(s) found for search term \"#{search_term}\":").green.bright
-  end
-
-  def self.find_jobs(type, search_term)
-    # Returns a multidimensional array that contains formatted output
-    # for each job found when searching by each job obj's location attr
-    selected_jobs = []
-    @@search_returned_job_ids.clear
-    case type
-    when 'location'
-      @@all.each do |job_obj|
-        if job_obj.location.include?(search_term)
-          selected_jobs << ["#{job_obj.id}. #{job_obj.title} located in #{job_obj.location}.", "Apply at: #{job_obj.apply_url}"]
-          @@search_returned_job_ids << job_obj.id
-        end
-      end
-    end
-    selected_jobs
-  end
-
-  def self.job_details(passed_job_id)
-    # Method is called and argument (Job ID) passed from CLI
-    # Ensures job ID exists, shows job details and returns to sub_menu
-    if @@search_returned_job_ids.include?(passed_job_id.to_i)
-      chosen_job = @@all.detect { |job| job.id == passed_job_id.to_i}
-      puts Rainbow("Job ID #{passed_job_id} chosen.").green.bright
-      detailed_job_output(chosen_job)
-      puts ""
-      CLI.sub_menu
-    else
-      puts Rainbow('Invalid selection! Please try again.').red.bright
-      CLI.sub_menu
-    end
-  end
-
-  def self.detailed_job_output(job)
-    puts "ID: #{job.id}"
-    puts "Position: #{job.title}"
-    puts "Location:  #{job.location}"
-    puts "Perks: #{job.perks}"
-    puts "Posted by employeer at: #{job.post_date}"
-    puts "Apply at: #{job.apply_url}"
   end
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,6 @@
 class CLI
-  @top_level_menu_options = [1, 2]
+  @menu_options = [1, 2]
+  @jobs_found_by_search = nil
  
   def self.run
     display_logo
@@ -16,7 +17,13 @@ class CLI
     puts '2. Exit'
     print 'Please choose an option using a number: '
     @input = gets.chomp.to_i
-    run_selection(@input) if validate_menu_input(@input) == true
+    if @menu_options.include?(@input)
+      run_selection(@input)
+    else
+      clear_terminal
+      puts Rainbow('Invalid selection! Please try again.').red.bright
+      menu
+    end
   end
 
   def self.run_selection(input)
@@ -25,7 +32,8 @@ class CLI
       clear_terminal
       print 'Please enter a location: '
       search_term = gets.chomp.to_s
-      Job.search('location', search_term)
+      Job.search(search_term)
+      Job.jobs_found? ? short_description(Job.jobs_found_by_search) : no_jobs_found
       sub_menu
     when 2
       puts 'Goodbye.'
@@ -33,25 +41,46 @@ class CLI
     end
   end
 
-  def self.validate_menu_input(input)
-    if @top_level_menu_options.include?(input)
-      true
-    else
+  def self.sub_menu
+    print "Enter job ID for more details or 'return' to conduct a new search or exit: "
+    input = gets.chomp
+    if input == 'return'
+      Job.clear_search_returned_jobs
       clear_terminal
-      puts Rainbow('Invalid selection! Please try again.').red.bright
       menu
+    else
+      if Job.return_job_by_index(input) != nil && input.to_i > 0
+        description(Job.return_job_by_index(input))
+      else
+        invalid_sub_menu_selection
+      end
+      sub_menu
     end
   end
 
-  def self.sub_menu
-    print "Enter job ID or 'return' to go back to the main menu: "
-    input = gets.chomp
-    if input == 'return'
-      clear_terminal
-      menu
-    else
-      Job.job_details(input)
+  def self.short_description(jobs)
+    jobs.each.with_index(1) do |job, index|
+      puts "#{index}. #{job.title} located in #{job.location}."
+      puts "------"
+      sleep(0.5)
     end
+  end
+
+  def self.description(job_obj)
+    puts Rainbow("#{job_obj.title}").green.bright
+    puts "Job is located in: #{job_obj.location}"
+    puts "Posted by employer: #{job_obj.post_date}"
+    puts "Apply online at #{job_obj.apply_url}"
+  end
+
+  def self.no_jobs_found
+    clear_terminal
+    puts Rainbow('No jobs found! Please try another search.').red.bright
+    menu 
+  end
+
+  def self.invalid_sub_menu_selection
+    puts Rainbow('Invalid job ID chosen! Please try again.').red.bright
   end
 
   def self.clear_terminal
